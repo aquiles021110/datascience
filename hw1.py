dd5541fd77f5ec287a8dbf1abe3c7dc188c397f2
aquiles021110 <aquiles@bousigues.com>
2026-06-10 15:12:44 +0200

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -8,3 +8,25 @@ elif op=='-':
     print(m1-m2)
 else:
     print('unkown operator')
+###
+print('###')
+a1=np.arange(16)
+print(a1)
+print(a1[a1<8],f'Values:{len(a1[a1<8])}')
+print(a1[a1>=10],f'Values:{len(a1[a1>=10])}')
+print(a1[a1%2!=0],f'Values:{len(a1[a1%2!=0])}')
+print('###')
+###
+m3=np.arange(1,10).reshape(3,3)
+m4=np.arange(10,19).reshape(3,3)
+print(m3)
+print(m4)
+p=m3*m4
+matrixmul=m3@m4
+print(p)
+print(matrixmul)
+###
+mat1=np.array([[1,1],[1,1]])
+mat2=np.arange(1,5).reshape(2,2)
+print(mat1@mat2)
+print(mat1*mat2)
