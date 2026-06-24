faa2820b22e46c426c5c5a695d9fb416eac1cbca
aquiles021110 <aquiles@bousigues.com>
2026-06-24 15:54:51 +0200

    Enhance data analysis in panda2.py
    Added data manipulation and analysis operations using pandas, including filtering, renaming columns, and saving to CSV.

--- panda2.py
+++ panda2.py
@@ -19,3 +19,30 @@ passenger_class2_3=df[df['Pclass'].isin([2,3])]
 print(passenger_class2_3[['Name','Pclass']])
 ###
 #pratice .loc
+print('###')
+print(df.iloc[9:25,2:5])
+print('###')
+print(df.loc[df['Age']>18,'Name'])
+#change value
+df.iloc[0:3,2]='Samuel'
+print(df['Name'])
+#make csv file
+print('###')
+df.to_csv('fake_titantic.csv')
+###
+df['Swimmer']=df['Fare']+2
+print(df['Swimmer'])
+df['New Fare']=df['Fare']*df['Pclass']
+print(df['New Fare'])
+#rename column
+df_renamed=df.rename(columns={'Pclass':'Passenger Class','Fare':'Fares'})
+print(df_renamed.info())
+#mean value of 2 columns
+print('###')
+print(df[['Age','Fare']].mean())
+print('###')
+#affect the same operation on multiple columns
+print(df.groupby('Age')['Fare'].mean())
+#aggregation
+print('###')
+print(df.groupby('Age')['Fare'].agg(['sum','mean','max']))
